696863d574d6744ccab1779c0e937f89f276c79d
Frank Shen <cmsflash99@gmail.com>
2017-03-29 21:24:59 +0800

    Review app member

--- member/models.py
+++ member/models.py
@@ -1,58 +1,56 @@
 from django.db import models
-from datetime import datetime
+from datetime import date
 
 class Member(models.Model):
         
-	username = models.CharField(max_length=50)
-	password = models.CharField(max_length=30)
-	nickname = models.CharField(max_length=30)
-	email = models.CharField(max_length=100)
-	total_amount = models.CharField(max_length=100)
-	paymentMethod = models.ForeignKey(PaymentMethod, on_delete = models.CASCADE)
-	
-	def get_rewards(self):
-		rewards = Reward.objects.filter(member=self)
-		now = time.time()
-		for reward in rewards:
-			if reward.expiration_date < now
-				Reward.objects.get(expiration_date=reward.expiration_date, member=self).delete()
-		return len(rewards)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    total_amount = models.IntegerField()
+    paymentMethod = models.ForeignKey(PaymentMethod, on_delete = models.CASCADE)
 
-	def use_rewards(number):
-		rewards = Reward.objects.filter(member=self)
-		now = datetime.now()
-		for reward in rewards:
-			if reward.expiration_date < now
-				Reward.objects.get(expiration_date=reward.expiration_date, member=self).delete()
-		sort(rewards, key=lambda reward: reward.expiration_date)
-		for reward in range(number):
-			Reward.objects.get(expiration_date=reward.expiration_date,member=self).delete()
-	
-	def accumulate_spending(amount):
-		total_amount = total_amount+amount
-		if total_amount>100:
-			now = datetime.now()
-			new_reward = Reward(now)
-			new_reward.save()
+    def __init__(self, username, password, nickname, email):
+        self.username = username
+        self.password = password
+        self.nickname = nickname
+        self.email = email
+        self.total_amount = 0
+        
+    def get_rewards(self):
+        rewards = Reward.objects.filter(member=self)
+        for reward in rewards:
+            if reward.expiration_date < date.today()
+                Reward.objects.get(expiration_date=reward.expiration_date, member=self).delete()
+        return len(rewards)
+
+    def use_rewards(number):
+        rewards = Reward.objects.filter(member=self)
+        for reward in rewards:
+            if reward.expiration_date < date.today()
+                Reward.objects.get(expiration_date=reward.expiration_date, member=self).delete()
+        sort(rewards, key=lambda reward: reward.expiration_date)
+        for reward in rewards[0:number]:
+            Reward.objects.get(expiration_date=reward.expiration_date, member=self).delete()
+    
+    def accumulate_spending(amount):
+        total_amount = total_amount+amount
+        if total_amount>100:
+            new_reward = Reward(date.today())
+            new_reward.save()
 
 
 
-		
+        
 class PaymentMethod(models.Model):
-	account_number = models.CharField(max_length=50)
+
+    account_number = models.IntegerField()
+
+    def __init__(self, account_number):
+        self.account_number = account_number
 
 class Reward(models.Model):
-	expiration_date = models.CharField(max_length=100)
-	member = models.ForeignKey(Member, on_delete = models.CASCADE)
-	def __init__(self,date):
-		self.expiration_date = date
-
-
-class PurchaseRecord(models.Model):
-
-	member = models.ForeignKey(Member, on_delete = models.CASCADE)
-	game_object = models.ForeighnKey(GameProduct, on_delete = models.CASCADE)
-
-	def __init__(self, member, game_product):
-		self.member = member
-		self.game_object = game_product
+    expiration_date = models.DateTimeField()
+    member = models.ForeignKey(Member, on_delete = models.CASCADE)
+    def __init__(self,date):
+        self.expiration_date = date
