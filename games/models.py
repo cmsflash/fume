9aa6965ccaa670d91eb790eb65d83d4ffb36b3aa
Frank Shen <cmsflash99@gmail.com>
2017-03-29 13:27:09 +0800

    Merge detail_page into games

--- games/models.py
+++ games/models.py
@@ -1,48 +1,64 @@
 from django.db import models
 
 class Game(models.Model):
-	ACTION = 'AC'
-	ADVENTURE = 'AD'
-	RACING = 'RA'
-	RPG = 'RP'
-	SIMULATION = 'SI'
-	SPORTS = 'SP'
-	STRATEGY = 'ST'
-	GENRES = (
-		(ACTION, 'Action'),
-		(ADVENTURE, 'Adventure'),
-		(RACING, 'Racing'),
-		(RPG, 'RPG'),
-		(SIMULATION, 'Simulation'),
-		(SPORTS, 'Sports'),
-		(STRATEGY, 'Strategy')
-	)
-	genre = models.CharField(max_length=2, choices=GENRES)
-	title = models.CharField(max_length=100, unique=True)
-	description = models.CharField(max_length=100)
-	detail = models.TextField()
-	thumbnail = models.ImageField(upload_to='images/thumbnails')
-	cover = models.ImageField(upload_to='images/covers')
+    ACTION = 'AC'
+    ADVENTURE = 'AD'
+    RACING = 'RA'
+    RPG = 'RP'
+    SIMULATION = 'SI'
+    SPORTS = 'SP'
+    STRATEGY = 'ST'
+    GENRES = (
+        (ACTION, 'Action'),
+        (ADVENTURE, 'Adventure'),
+        (RACING, 'Racing'),
+        (RPG, 'RPG'),
+        (SIMULATION, 'Simulation'),
+        (SPORTS, 'Sports'),
+        (STRATEGY, 'Strategy')
+    )
+    genre = models.CharField(max_length=2, choices=GENRES)
+    title = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=100)
+    detail = models.TextField()
+    thumbnail = models.ImageField(upload_to='images/thumbnails')
+    cover = models.ImageField(upload_to='images/covers')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Item(models.Model):
-	LINUX = 'L'
-	MACOS = 'M'
-	WINDOWS = 'W'
-	PLATFORMS = (
-		(LINUX, 'Linux'),
-		(MACOS, 'macOS'),
-		(WINDOWS, 'Windows')
-	)
-	platform = models.CharField(max_length=1, choices=PLATFORMS)
-	game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='items')
-	price = models.DecimalField(max_digits=5, decimal_places=2)
+    LINUX = 'L'
+    MACOS = 'M'
+    WINDOWS = 'W'
+    PLATFORMS = (
+        (LINUX, 'Linux'),
+        (MACOS, 'macOS'),
+        (WINDOWS, 'Windows')
+    )
+    platform = models.CharField(max_length=1, choices=PLATFORMS)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='items')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
-	def __str__(self):
-		return self.game.title + ' - ' + self.get_platform_display()
+    def __str__(self):
+        return self.game.title + ' - ' + self.get_platform_display()
 
-	class Meta:
-		unique_together = ["game", "platform"]
+    class Meta:
+        unique_together = ["game", "platform"]
+
+class Member(models.Model):
+    pass
+
+class GameProduct(models.Model):
+    pass
+
+class PurchaseRecord(models.Model):
+
+    member = models.ForeignKey(Member, on_delete = models.CASCADE)
+    game_object = models.ForeignKey(GameProduct, on_delete = models.CASCADE)
+
+    def __init__(self, member, game_product):
+        self.member = member
+        self.game_object = game_product
+
