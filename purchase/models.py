502e2c0993516e8a0ec26cd81fd99baa3f932ce9
Frank Shen <cmsflash99@gmail.com>
2017-04-10 10:54:21 +0800

    Add date_time field to PurchaseRecord

--- purchase/models.py
+++ purchase/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from member.models import Member
 from games.models import Item
+from datetime import datetime
 
 class PurchaseManager(models.Manager):
 
     def purchase_records_of(member):
-        return list(self.filter(member=member))
+        return list(self.filter(member=member).order_by('-date_time'))
 
 class PurchaseRecord(models.Model):
 
     member = models.ForeignKey(Member, on_delete = models.CASCADE, related_name='purchase_records')
     item = models.ForeignKey(Item, on_delete = models.CASCADE, related_name='purchase_records')
+    date_time = models.DateTimeField()
     objects = PurchaseManager()
     
     @classmethod
-    def create(cls, member, item):
-        return cls(member=member, item=item)
+    def create(cls, member, item, date_time):
+        return cls(member=member, item=item, date_time=date_time)
