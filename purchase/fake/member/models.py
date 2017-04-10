727ad18f9fdea12668d29fc4a49267dc8bac2bf8
Frank Shen <cmsflash99@gmail.com>
2017-04-10 10:46:30 +0800

    Add fake apps in tags and purchase

---
+++ purchase/fake/member/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from datetime import date
+
+class Member(models.Model):
+        
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    total_amount = models.FloatField()
+
+    @classmethod
+    def create(cls, username, password, nickname, email, payment_method):
+        member = cls(username=username, password=password, nickname=nickname, email=email, total_amount=0, paymentMethod = payment_method)
+        return member
+        
+    def get_rewards(self):
+        rewards = Reward.objects.filter(member=self)
+        for reward in rewards:
+            if reward.expiration_date < date.today():
+                Reward.objects.get(expiration_date=reward.expiration_date, member=self).delete()
+        return len(rewards)
+
+    def use_rewards(self, number):
+        rewards = self.rewards.all()
+        for reward in rewards:
+            if reward.expiration_date < date.today():
+                reward.delete()
+        used_rewards = rewards.order_by('expiration_date')[:number]
+        for reward in used_rewards:
+            reward.delete()
+    
+    def accumulate_spending(self, amount):
+        self.total_amount += amount
+        while self.total_amount > Reward.THRESHOLD:
+            new_reward = Reward.create(self, date.today())
+            new_reward.save()
+            self.total_amount -= Reward.THRESHOLD
+        self.save()
+
+class PaymentMethod(models.Model):
+
+    account_number = models.IntegerField()
+    member = models.OneToOneField(Member, on_delete = models.CASCADE, related_name='payment_method')
+
+    @classmethod
+    def create(cls, account_number, member):
+        return cls(account_number=account_number, member=member)
+
+class Reward(models.Model):
+
+    THRESHOLD = 100
+    
+    expiration_date = models.DateField()
+    member = models.ForeignKey(Member, on_delete = models.CASCADE, related_name='rewards')
+    @classmethod
+    def create(cls, member, date):
+        return cls(expiration_date = date, member=member)
--- purchase/models.py
+++ purchase/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from member.models import Member
 from games.models import Item
 
+class PurchaseManager(models.Manager):
+
+    def purchase_records_of(member):
+        return list(self.filter(member=member))
+
 class PurchaseRecord(models.Model):
 
     member = models.ForeignKey(Member, on_delete = models.CASCADE, related_name='purchase_records')
     item = models.ForeignKey(Item, on_delete = models.CASCADE, related_name='purchase_records')
+    objects = PurchaseManager()
     
     @classmethod
     def create(cls, member, item):
