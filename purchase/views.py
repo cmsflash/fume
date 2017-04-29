0b0ccebbfd2df0d6e76127a0bb8facbced80264e
Frank Shen <cmsflash99@gmail.com>
2017-04-29 13:30:36 +0800

    Update purchase.views with @login_required

--- purchase/views.py
+++ purchase/views.py
@@ -5,23 +5,22 @@ from .classes import Purchase, Payment
 from .models import PurchaseRecord
 from member.models import Member, PaymentMethod
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def purchase(request, game_product_id):
     game_product = Item.objects.get(pk=game_product_id)
-    if not request.user.is_authenticated():
-        return HttpResponse('Please log in or sign up first')
     member = request.user.member
     purchase = Purchase(member, game_product)
     context = {'member':member.nickname, 'game_product': str(game_product), 'game_product_id':game_product_id, 'reward_count':min(member.get_number_of_rewards(), 10)}
     return render(request, 'purchase/purchase.html', context)
 
+@login_required
 def pay(request, game_product_id):
     rewards_to_use = int(request.GET.get('rewards_to_use'))
     item = Item.objects.get(pk=game_product_id)
-    if not request.user.is_authenticated():
-        return HttpResponse('Please log in or sign up first')
     member = request.user.member
     payment_method = member.payment_method
     payment = Payment(payment_method)
@@ -35,9 +34,11 @@ def pay(request, game_product_id):
     context = {'successful':successful, 'amount':round(amount, 2), 'game_product':str(item), 'game':item.game.pk}
     return render(request, 'purchase/pay.html', context)
 
+@login_required
 def clear(request, game_id):
     game = Game.objects.get(pk=game_id)
-    items = game.items.all()
+    items = game.items.filter(member=member)
+    member = request.user.member
     for item in items:
         item.purchase_records.all().delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
