6bbf3254a29dafbeee471df9f54e5ba2d6957457
Frank Shen <cmsflash99@gmail.com>
2017-04-29 19:26:46 +0800

    Changed accumulated_spending at index page to speding_needed

--- fume/views.py
+++ fume/views.py
@@ -7,7 +7,7 @@ def index(request):
     featured_games = [featured_game.game for featured_game in FeaturedGame.objects.all()]
     if request.user.is_authenticated():
         member = request.user.member
-        context = {'recommendations':Recommender.make_recommendations(member), 'featured_games': featured_games}
+        context = {'recommendations':Recommender.make_recommendations(member), 'featured_games': featured_games, 'spending_needed': 100 - member.accumulated_spending}
     else:
         context = {'authenticated':False, 'featured_games': featured_games}
     return render(request, 'fume/index.html', context)
