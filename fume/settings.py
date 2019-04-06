df53d6585acd79eb0633a00b550c27d9db68ef9d
Shen Zhuoran <cmsflash99@gmail.com>
2019-04-06 19:14:39 +0800

    Use django_heroku package

--- fume/settings.py
+++ fume/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -197,3 +200,5 @@ SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
 }
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+django_heroku.settings(locals())
