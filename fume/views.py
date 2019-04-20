ccf29d0ded5687354994b9ac9d0364e4059ec9ca
Shen Zhuoran <cmsflash99@gmail.com>
2019-04-20 20:53:57 +0800

    Stylize fume

--- fume/views.py
+++ fume/views.py
@@ -3,11 +3,18 @@ from member.models import Member
 from games.models import FeaturedGame
 from recommendation.classes import Recommender
 
+
 def index(request):
-    featured_games = [featured_game.game for featured_game in FeaturedGame.objects.all()]
+    featured_games = [
+        featured_game.game for featured_game in FeaturedGame.objects.all()
+    ]
     if request.user.is_authenticated:
         member = request.user.member
-        context = {'recommendations':Recommender.make_recommendations(member), 'featured_games': featured_games, 'spending_needed': 100 - member.accumulated_spending}
+        context = {
+            'recommendations':Recommender.make_recommendations(member),
+            'featured_games': featured_games,
+            'spending_needed': 100 - member.accumulated_spending
+        }
     else:
         context = {'authenticated':False, 'featured_games': featured_games}
     return render(request, 'fume/index.html', context)
